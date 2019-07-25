f7f4a08ac4bb2d230b7c653e594b3ce30cca8fdb
Josh Gamache <jsgamache@gmail.com>
2019-07-25 18:07:16 +0000

    The most exciting application ever created!

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+    res.render("home");
+});
+
+app.get("/about", (req,res) => {
+    res.render("about");
+});
+
+// Server start!
+app.listen(process.env.PORT, () => {
+    console.log("Server started!");
+})
